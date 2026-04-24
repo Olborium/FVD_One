eef2598fecc05c9bfe98bafcf136e0ff548eeaf8
Andrey Shkerin <149336823+Olborium@users.noreply.github.com>
2026-04-24 17:39:18 -0400

    Create oscs_phi4.py

---
+++ oscs_phi4.py
@@ -0,0 +1,95 @@
+#%%
+import sys
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+})
+
+#%%
+#SIZE = 100
+#N_x = 2048
+#DX = SIZE/N_x
+
+eta = 0
+
+TempList = [0.05]
+M = len(TempList)
+
+decays_tot = np.zeros(M)
+decays_osc = np.zeros(M)
+
+path = 'Phi4'
+files = os.listdir(path)
+
+times = []
+oscs = []
+
+for i in range(M):
+
+    TEMP = TempList[i]
+    times_i_list = []
+    oscs_i_list = []
+    temp_str = f'{TEMP:.6f}'
+    eta_str = f'{eta:.6f}'
+    times_pattern = re.compile(rf'^times_{re.escape(temp_str)}_(-?\d+)\.txt$')
+    osc_pattern = re.compile(rf'^osc_counter_{re.escape(temp_str)}_(-?\d+)\.txt$')
+
+    osc_files = {}
+    for filename in files:
+        match = osc_pattern.match(filename)
+        if match:
+            osc_files[match.group(1)] = filename
+
+    for filename in sorted(files):
+
+        match = times_pattern.match(filename)
+        if not match:
+            continue
+
+        seed = match.group(1)
+        osc_filename = osc_files.get(seed)
+        if osc_filename is None:
+            print(f"Missing osc_counter file for TEMP={temp_str}, seed={seed}")
+            continue
+
+        times_filepath = os.path.join(path, filename)
+        osc_filepath = os.path.join(path, osc_filename)
+           
+        times_i_list.append(np.loadtxt(times_filepath))
+        oscs_i_list.append(np.loadtxt(osc_filepath))
+
+    times += [np.concatenate(times_i_list)]
+    oscs += [np.concatenate(oscs_i_list)]
+
+#%%
+
+J = 0
+
+oscs_J = oscs[J][times[J]>0] / 4.0
+
+#%%
+
+values, counts = np.unique(oscs_J, return_counts=True)
+abundance = counts / len(oscs_J)
+
+#%%
+""" Make a histogram """
+
+values, counts = np.unique(oscs_J, return_counts=True)
+abundance = counts / len(oscs_J)
+
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.bar(values[0], abundance[0], width=0.4, color='blue', alpha=0.7)
+ax.bar(values[1:], abundance[1:], width=0.4, color='red', alpha=0.7)
+ax.set_xlabel(r'$J$', fontsize=14)
+ax.set_ylabel(r'$n_J$', fontsize=14)
+ax.grid(True, axis='y', linestyle='dashed', linewidth=0.5, color='grey')
+ax.tick_params(axis='both', which='both', right=True, top=True, direction='in', labelsize=12)
+plt.xlim((-0.2,7.2))
+#plt.savefig('Phi4_Nosc_T=0.05.pdf', bbox_inches='tight')
+plt.show()
